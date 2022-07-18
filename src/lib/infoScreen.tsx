be97297e8447277e231056190ab5abdc79616ded
Eduardo M Gomes <eduardo.2001.06@gmail.com>
2022-07-18 10:08:25 -0300

    netinfo probe id

--- src/lib/infoScreen.tsx
+++ src/lib/infoScreen.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { Button, Text, View } from "react-native";
+import { Button, Text, TextInput, View } from "react-native";
 import { RootStackParamList } from "../App";
 
 import { networking } from "./socket";
@@ -9,12 +9,20 @@ import ID from "./id";
 
 export default function InfoScreen() {
 	const addr = networking.getAddress();
+	const [ip, onChangeIp] = React.useState("10.0.2.200");
+	const [port, onChangePort] = React.useState("5000");
+	const [gotId, onGetId] = React.useState<string | null>(null);
 
 	return (
 		<View>
 			<Text>Info screen</Text>
 			<Text>{JSON.stringify(addr)}</Text>
-			<Button title="Send hello" onPress={networking.sendHello}/>
+			<Text>IP:</Text><TextInput value={ip} onChangeText={onChangeIp}></TextInput>
+			<Text>Port:</Text><TextInput value={port} onChangeText={onChangePort}></TextInput>
+			<Button title="Send hello" onPress={() => {
+				networking.probeId(ip, Number(port)).then(onGetId);
+			}} />
+			{gotId ? <Text>Id: {gotId}</Text> : undefined}
 			<Button title="Reset ID" onPress={ID.resetId} />
 		</View>
 	);
@@ -23,7 +31,7 @@ export default function InfoScreen() {
 type Navigation<Screen extends keyof RootStackParamList> = NativeStackNavigationProp<RootStackParamList, Screen>;
 
 function InfoButton<Screen extends keyof RootStackParamList>(navigation: Navigation<Screen>) {
-	return (<Button onPress={() => {navigation.navigate("NetInfo");}} title="NetInfo"/>);
+	return (<Button onPress={() => { navigation.navigate("NetInfo"); }} title="NetInfo" />);
 }
 
-export {InfoButton};
+export { InfoButton };
