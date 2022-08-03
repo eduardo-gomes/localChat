df6c88de1bb500e33221b37fd22ee42a7de0840c
Eduardo M Gomes <eduardo.2001.06@gmail.com>
2022-08-03 00:58:38 -0300

    Create types for file message

--- src/lib/messageTransmitter.ts
+++ src/lib/messageTransmitter.ts
@@ -2,7 +2,7 @@
 import { MMKVLoader, useMMKVStorage } from "react-native-mmkv-storage";
 
 import type { ContactInfo } from '../lib';
-import { MessageTypes, TextMessage, TextMessageAck } from "./netMessages";
+import { MessageTypes, TextMessage, TextMessageAck, FileMessage } from "./netMessages";
 import { sendIfConnected } from "./messageConnectedTransmit";
 
 const messages = new MMKVLoader().withInstanceID("messages").initialize();
@@ -19,9 +19,19 @@ interface MessageWithId extends Message {
 interface StoredMessage extends MessageWithId {
 	local: boolean
 };
+type StoredFileMessage = {
+	path: string,
+	id: string,
+	name: string,
+	size: number,
+	isFile: true,
+	sent?: boolean,
+	local: boolean,
+};
+type StoredAnyMessage = StoredFileMessage | StoredMessage;
 
 function useMessages(contact: ContactInfo) {
-	const [message] = useMMKVStorage<StoredMessage[]>(contact.uid, messages, []);
+	const [message] = useMMKVStorage<StoredAnyMessage[]>(contact.uid, messages, []);
 	return message
 }
 
@@ -34,8 +44,8 @@ function sendMessage(contact: ContactInfo, messageSource: Message) {
 	sendIfConnected(contact.uid);
 }
 
-function appendMessage(uid: string, message: StoredMessage) {
-	let array = messages.getArray<StoredMessage>(uid) ?? [];
+function appendMessage(uid: string, message: StoredAnyMessage) {
+	let array = messages.getArray<StoredAnyMessage>(uid) ?? [];
 	array.push(message);
 	messages.setArray(uid, array);
 }
@@ -52,15 +62,15 @@ function receiveMessage(uid: string, textMessage: TextMessage) {
 
 import { Connection } from "./socket";
 
-function getPending(contactId: string){
-	let array = messages.getArray<StoredMessage>(contactId) ?? [];
+function getPending(contactId: string) {
+	let array = messages.getArray<StoredAnyMessage>(contactId) ?? [];
 	return array.filter(msg => msg.local && msg.sent == false);
 }
 
 function unQueueMessage(contactId: string, messageId: string) {
-	let array = messages.getArray<StoredMessage>(contactId) ?? [];
+	let array = messages.getArray<StoredAnyMessage>(contactId) ?? [];
 	let found = array.find(msg => msg.local && msg.id == messageId);
-	if(!found) return;
+	if (!found) return;
 	found.sent = true;
 	messages.setArray(contactId, array);
 }
@@ -70,16 +80,26 @@ function sendToConnection(connection: Connection) {
 	const pending = getPending(uid);
 	console.log(`[Message manager] send to ${uid}, pendingMessages:`, pending);
 	pending.forEach((msg) => {
-		let netMsg: TextMessage = { type: MessageTypes.TEXT_MESSAGE, content: msg.content, id: msg.id };
-		connection.send(netMsg);
+		function toNetMessage(_msg: StoredAnyMessage) {
+			if ((_msg as StoredMessage).content) {
+				const msg = _msg as StoredMessage;
+				return { type: MessageTypes.TEXT_MESSAGE, content: msg.content, id: msg.id } as TextMessage;
+			} else {
+				const msg = _msg as StoredFileMessage;
+				return { type: MessageTypes.FILE_MESSAGE, id: msg.id, name: msg.name, size: msg.size } as FileMessage;
+			}
+		}
+		connection.send(toNetMessage(msg));
 	})
 }
-function onIncomingMessage({ origin, msg }: { origin: Connection, msg: TextMessage | TextMessageAck }) {
+function onIncomingMessage({ origin, msg }: { origin: Connection, msg: TextMessage | TextMessageAck | FileMessage }) {
 	const uid = origin.getPeerId()
 	if (msg.type == MessageTypes.TEXT_MESSAGE) {
 		// console.log("From", uid, "got message:", msg);
 		origin.send({ type: MessageTypes.TEXT_MESSAGE_ACK, id: msg.id } as TextMessageAck);
 		receiveMessage(uid, msg);
+	} else if (msg.type == MessageTypes.FILE_MESSAGE) {
+		console.log("Received file message:", msg);
 	} else
 		unQueueMessage(uid, msg.id);
 }
--- src/lib/netMessages.ts
+++ src/lib/netMessages.ts
@@ -1,12 +1,13 @@
 enum MessageTypes {
 	BANNER = "BANNER",
 	TEXT_MESSAGE = "MESSAGE_TXT",
+	FILE_MESSAGE = "MESSAGE_FILE",
+	FILE_MESSAGE_ACK = "FILE_ACK",
+	FILE_MESSAGE_REQUEST = "FILE_REQUEST",
 	TEXT_MESSAGE_ACK = "TXT_ACK"
 };
 
-interface NetMessage {
-	type: string
-};
+type NetMessage = BannerMessage | TextMessage | FileMessage | FileRequestMessage | FileAckMessage | TextMessageAck;
 
 type BannerMessage = {
 	type: MessageTypes.BANNER,
@@ -19,10 +20,28 @@ type TextMessage = {
 	id: string
 };
 
+type FileMessage = {
+	type: MessageTypes.FILE_MESSAGE,
+	name: string,
+	size: number,
+	id: string
+};
+
+type FileRequestMessage = { //peer asks for file, in response to FileMessage
+	type: MessageTypes.FILE_MESSAGE_REQUEST,
+	port: number, //Port where peer should send file
+	id: string
+};
+
+type FileAckMessage = { //Reports if file was successfully received
+	type: MessageTypes.FILE_MESSAGE_ACK,
+	id: string
+};
+
 type TextMessageAck = {
 	type: MessageTypes.TEXT_MESSAGE_ACK,
 	id: string
 };
 
-export type { NetMessage, BannerMessage, TextMessage, TextMessageAck };
+export type { NetMessage, BannerMessage, TextMessage, TextMessageAck, FileMessage, FileRequestMessage, FileAckMessage };
 export { MessageTypes };
