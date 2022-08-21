007bcc898eb5a579db68741f2ed4869670e599ff
Eduardo M Gomes <eduardo.2001.06@gmail.com>
2022-08-21 16:44:03 -0300

    zeroconf limit updates to auto_connect

--- src/lib/zeroconf.ts
+++ src/lib/zeroconf.ts
@@ -1,16 +1,34 @@
 import { useEffect, useState } from "react";
-import Zeroconf from "react-native-zeroconf";
+import Zeroconf, { Service } from "react-native-zeroconf";
+import { getContacts } from "./contactManager";
 import { getId } from "./id";
 
-type Discovered_Host = { id: string, address: string, port: number };
+type Discovered_Host = { id: string, address: string, port: number, alt?: string[] };
+
+function service_to_host(service: Service): Discovered_Host {
+	let alt = service.addresses.length > 1 ? service.addresses.slice(1) : undefined;
+	return ({ id: service.name, address: service.addresses[0], port: service.port, alt })
+}
+
+function auto_connect(hosts: Discovered_Host[]) {
+	const know_hosts = getContacts();
+	const know_available = hosts.filter((host) => know_hosts.hasOwnProperty(host.id));
+	console.log("[zeroconf] update: ", JSON.stringify(hosts));
+	console.log("[zeroconf] known: ", JSON.stringify(know_available));
+}
 
 class Zeroconf_App {
 	zeroconf: Zeroconf;
+	timeout?: NodeJS.Timeout;
 	constructor() {
 		this.zeroconf = new Zeroconf();
 		this.zeroconf.scan("local-chat", "tcp", "local.");
-		this.zeroconf.on("resolved", (service) => {
-			console.log("[zeroconf] got: ", JSON.stringify(service, undefined, 4));
+		this.zeroconf.on("update", () => {
+			let got = this.getAvailable();
+			if (this.timeout) {
+				clearTimeout(this.timeout);
+			}
+			this.timeout = setTimeout(auto_connect, 3000, got);
 		})
 	}
 	publish(port: number) {
@@ -21,10 +39,10 @@ class Zeroconf_App {
 		});
 	}
 	addListener(service_listener: (discovered: Discovered_Host) => void) {
-		this.zeroconf.on("resolved", (service) => { service_listener({ id: service.name, address: service.addresses[0], port: service.port }) });
+		this.zeroconf.on("resolved", (service) => { service_listener(service_to_host(service)) });
 	}
 	getAvailable(): Discovered_Host[] {
-		let available = Object.entries(this.zeroconf.getServices()).map<Discovered_Host>(([name, service]) => ({ id: name, address: service.addresses[0], port: service.port }));
+		let available = Object.entries(this.zeroconf.getServices()).filter((e) => e[1].addresses != undefined).map<Discovered_Host>(([name, service]) => service_to_host(service));
 		return available;
 	}
 	useAvailable() {
